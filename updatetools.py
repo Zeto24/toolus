36e1e09dc05659596944e41142243da407a80128
Zeto24 <109522475+Zeto24@users.noreply.github.com>
2022-07-31 01:42:42 +0700

    Add files via upload

--- updatetools.py
+++ updatetools.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import time
-os.system("rm -f toolus")
+os.system("rm -rf toolus")
 os.system("git clone https://github.com/Zeto24/toolus.git")
 os.system("clear")
 print("-"*30)
