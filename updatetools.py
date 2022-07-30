446214c5a7af650de15c091c38f8ccf269e62540
Zeto24 <109522475+Zeto24@users.noreply.github.com>
2022-07-31 01:40:23 +0700

    Add files via upload

--- updatetools.py
+++ updatetools.py
@@ -3,5 +3,9 @@ import sys
 import time
 os.system("rm -f toolus")
 os.system("git clone https://github.com/Zeto24/toolus.git")
+os.system("clear")
+print("-"*30)
 print("Tools telah diupdate!")
 time.sleep(1)
+os.system("cd toolus")
+os.system("python toolus.py")
